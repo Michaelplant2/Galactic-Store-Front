0cb7ba7939729effc0b92933bcbebb3a6228282d
Michael Plantamura <105463635+Michaelplant2@users.noreply.github.com>
2023-09-01 16:48:45 -0400

    setting up header and footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Container } from 'react-bootstrap'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
 
@@ -10,7 +12,12 @@ function App() {
     <>
       <div>
         <Header />
-        My App
+        <main className="py-3">
+          <Container>
+            <h1>Welcome</h1>
+          </Container>
+        </main>
+        <Footer />
       </div>
     </>
   )
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,12 +1,17 @@
+import { Container, Row, Col } from 'react-bootstrap'
 import React from 'react'
 
 function Footer() {
 
   return (
    <>
-   <div>
-      <footer>Footer</footer>
-   </div>
+    <footer>
+      <Container>
+        <Row>
+          <Col className="text-center py-3">Copyright &copy; Galactic Store Front</Col>
+        </Row>
+      </Container>
+    </footer>
    </>
   )
 }
